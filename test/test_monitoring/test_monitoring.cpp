a1067c6da58c32be7ebb1d77737569b4bc5f4172
Jiří Píša <jirka.pisa@gmail.com>
2026-06-03 00:46:40 +0200

    feat(render): extract scaleRgb565 + channelColor to Color.h

--- test/test_monitoring/test_monitoring.cpp
+++ test/test_monitoring/test_monitoring.cpp
@@ -1,5 +1,6 @@
 #include <unity.h>
 
+#include "core/render/Color.h"
 #include "core/render/KeyLayout.h"
 
 void setUp() {}
@@ -25,10 +26,27 @@ static void test_out_of_range_note_has_invalid_rect() {
     TEST_ASSERT_EQUAL_INT(-1, r.x);
 }
 
+static void test_scale_full_is_identity() {
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, core::scaleRgb565(0xFFFF, 256));
+}
+static void test_scale_zero_is_black() {
+    TEST_ASSERT_EQUAL_HEX16(0x0000, core::scaleRgb565(0xFFFF, 0));
+}
+static void test_channel0_is_white() {
+    TEST_ASSERT_EQUAL_HEX16(core::color::White, core::channelColor(0));
+}
+static void test_channels_have_distinct_colors() {
+    TEST_ASSERT_NOT_EQUAL(core::channelColor(1), core::channelColor(2));
+}
+
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_white_key_index_roundtrip);
     RUN_TEST(test_c2_is_white_first_key);
     RUN_TEST(test_out_of_range_note_has_invalid_rect);
+    RUN_TEST(test_scale_full_is_identity);
+    RUN_TEST(test_scale_zero_is_black);
+    RUN_TEST(test_channel0_is_white);
+    RUN_TEST(test_channels_have_distinct_colors);
     return UNITY_END();
 }
